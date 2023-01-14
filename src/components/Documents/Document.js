eb2101d4675586b14a5bcf2f0e9b6d23282cb2c5
Roman Liush <romanliush24@gmail.com>
2023-01-14 13:12:19 +0200

    delete

--- src/components/Documents/Document.js
+++ src/components/Documents/Document.js
@@ -1,44 +1,74 @@
 import React, {useState, useEffect} from "react";
 import './style.css';
+import {Link} from "react-router-dom";
 
 import Plus from '../../assets/plus.png';
 import Logo from '../HeaderLogo/Logo';
 import Edit from "./EditFile/EditFile";
-import {Link} from "react-router-dom";
+import Dots from '../../assets/dots.svg';
+import Garbage from '../../assets/garbage.svg';
+
 
 const Document = () => {
 
     const Docs = [
         {
-            'text': '1 Lorem Ipsum and others are simply dummy pictures and videos with caption and details of their'
+            'id': 1,
+            'text': '1 Lorem Ipsum and others are simply dummy pictures and videos with caption and details of their',
+            'open': false
         },
         {
-            'text': '2 Lorem Ipsum and others are simply dummy pictures and videos with caption and details of their'
+            'id': 2,
+            'text': '2 Lorem Ipsum and others are simply dummy pictures and videos with caption and details of their',
+            'open': false
         },
         {
-            'text': '3 Lorem Ipsum and others are simply dummy pictures and videos with caption and details of their'
+            'id': 3,
+            'text': '3 Lorem Ipsum and others are simply dummy pictures and videos with caption and details of their',
+            'open': false
         },
         {
-            'text': '4 Lorem Ipsum and others are simply dummy pictures and videos with caption and details of their'
+            'id': 4,
+            'text': '4 Lorem Ipsum and others are simply dummy pictures and videos with caption and details of their',
+            'open': false
         },
         {
-            'text': '5 Lorem Ipsum and others are simply dummy pictures and videos with caption and details of their'
+            'id': 5,
+            'text': '5 Lorem Ipsum and others are simply dummy pictures and videos with caption and details of their',
+            'open': false
         },
         {
-            'text': '6 Lorem Ipsum and others are simply dummy pictures and videos with caption and details of their'
+            'id': 6,
+            'text': '6 Lorem Ipsum and others are simply dummy pictures and videos with caption and details of their',
+            'open': false
         },
         {
-            'text': '7 Lorem Ipsum and others are simply dummy pictures and videos with caption and details of their'
+            'id': 7,
+            'text': '7 Lorem Ipsum and others are simply dummy pictures and videos with caption and details of their',
+            'open': false
         },
         {
-            'text': '8 Lorem Ipsum and others are simply dummy pictures and videos with caption and details of their'
+            'id': 8,
+            'text': '8 Lorem Ipsum and others are simply dummy pictures and videos with caption and details of their',
+            'open': false
         },
         {
-            'text': '9 Lorem Ipsum and others are simply dummy pictures and videos with caption and details of their'
+            'id': 9,
+            'text': '9 Lorem Ipsum and others are simply dummy pictures and videos with caption and details of their',
+            'open': false
         }
     ];
 
     const [listOfDocs, setListOfDocs] = useState(Docs);
+    const [isDelete, setIsDelete] = useState(false);
+
+    console.log('***************8888****************')
+    console.log(listOfDocs)
+    console.log('***************8888****************')
+
+    const handleClick = (id) => {
+        setIsDelete(isDelete === id ? -1 : id );
+    }
 
     return (
         <main className={'document_page'}>
@@ -54,11 +84,41 @@ const Document = () => {
                 <input type="file" name="photo" id="upload-photo"/>
 
                 {
-                    listOfDocs.map((val, index) => <Link to={'/edit'}>
-                        <section className={'listOfDocs'} key={index}>
-                            <p>{val.text.split(' ', 5).join(' ') + '...'}</p>
-                        </section>
-                    </Link>)
+                    listOfDocs.map((val, index) =>
+                        <section className={'general_block'}>
+                            <Link to={'/edit'}>
+                                <section className={'listOfDocs'} key={index}>
+                                    <p>{val.text.split(' ', 5).join(' ') + '...'}</p>
+
+                                </section>
+                            </Link>
+                            <img src={Dots}
+                                 alt={'dots'}
+                                 className={'listOfDocs_dots'}
+                                 onClick={(e) => {
+                                     // setIsDelete(!isDelete)
+                                     handleClick(val.id)
+                                     console.log(e, val)
+                                 }}
+                            />
+
+
+                                <section className={'listOfDocs_delete'}
+                                         style={{opacity: isDelete === val.id ? '1' : '0'}}
+                                         key={val.id}
+                                         onClick={(e) => {
+                                             // setIsDelete(!isDelete)
+                                             handleClick(val.id)
+                                             console.log(e, val)
+                                         }}
+                                >
+                                    <p>Delete</p>
+                                    <img src={Garbage}
+                                         alt={'garbage bucket'}
+                                    />
+                                </section>
+
+                        </section>)
                 }
             </section>
         </main>
